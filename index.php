3fd400f3c89b0dba27bb6df1ee84aa48c10b620b
SAIFAR BUNYANG <58024098@live.up.ac.th>
2018-04-03 21:51:36 +0700

    fix by bank

---
+++ index.php
@@ -0,0 +1,194 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="assets/img/favicon.png">  
+  
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>Get Shit Done Kit by Creative Tim</title>
+
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+  
+    <link href="bootstrap3/css/bootstrap.css" rel="stylesheet" />
+    <link href="bootstrap3/css/font-awesome.css" rel="stylesheet" />
+    
+  <link href="assets/css/gsdk.css" rel="stylesheet" />   
+    <link href="assets/css/demo.css" rel="stylesheet" /> 
+    <link href="assets/css/main.css" rel="stylesheet" /> 
+
+    <!--     Font Awesome     -->
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
+  
+</head>
+
+<body>
+ <div id="navbar-full">
+    <div id="navbar">
+    <!--    
+        navbar-default can be changed with navbar-ct-blue navbar-ct-azzure navbar-ct-red navbar-ct-green navbar-ct-orange  
+        -->
+        <nav class="navbar navbar-ct-blue navbar-transparent navbar-fixed-top" role="navigation">
+          <div class="alert alert-success hidden">
+          
+          </div>
+          
+          <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#gsdk">GSDKit</a>
+            </div>
+        
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#gsdk">Link</a></li>
+                <li class="dropdown">
+                      <a href="#gsdk" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                      <ul class="dropdown-menu">
+                        <li><a href="#gsdk">Action</a></li>
+                        <li><a href="#gsdk">Another action</a></li>
+                        <li><a href="#gsdk">Something</a></li>
+                        <li><a href="#gsdk">Another action</a></li>
+                        <li><a href="#gsdk">Something</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#gsdk">Separated link</a></li>
+                      </ul>
+                </li>
+                <li>
+                    <a href="javascript:void(0);" data-toggle="search" class="hidden-xs"><i class="fa fa-search"></i></a>
+                </li>
+              </ul>
+               <form class="navbar-form navbar-left navbar-search-form" role="search">                  
+                 <div class="form-group">
+                      <input type="text" value="" class="form-control" placeholder="Search...">
+                 </div> 
+              </form>
+              <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#gsdk">Register</a></li>
+                    
+                    <li><button href="#gsdk" class="btn btn-round btn-default">Sign in</button></li>
+               </ul>
+              
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+        <div class="blurred-container">
+            <div class="img-src" style="background-image: url('assets/img/bg.jpg')"></div>
+        </div>
+    </div><!--  end navbar -->
+
+</div> <!-- end menu-dropdown -->
+
+<div class="main">
+    <div class="container tim-container" style="max-width:800px; padding-top:100px">
+       <h1 class="text-center">Awesome looking header <br> just for my friends<small class="subtitle">This is like a small motto before the story.</small></h1>
+       <div class="container">
+  <div class="row">
+    
+ <div class="col-md-8">
+
+      <div class="panel panel-default">
+              <div class="panel-body">
+                <input type="text" class="form-control" placeholder="What are you up to?">
+              </div>
+                <div class="panel-footer">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-link btn-icon"><i class="fa fa-map-marker"></i></button>
+                    <button type="button" class="btn btn-link btn-icon"><i class="fa fa-users"></i></button>
+                    <button type="button" class="btn btn-link btn-icon"><i class="fa fa-camera"></i></button>
+                    <button type="button" class="btn btn-link btn-icon"><i class="fa fa-star"></i></button>
+                  </div>
+                  
+                  <div class="pull-right">
+                    <button type="button" class="btn btn-success">Post</button>
+                  </div>  
+                </div>
+            </div>
+          </div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="[ panel panel-default ] panel-google-plus">
+                <div class="dropdown">
+                    <span class="dropdown-toggle" type="button" data-toggle="dropdown">
+                        <span class="[ glyphicon glyphicon-chevron-down ]"></span>
+                    </span>
+                    <ul class="dropdown-menu" role="menu">
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
+                        <li role="presentation" class="divider"></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
+                    </ul>
+                </div>
+                <div class="panel-google-plus-tags">
+                    <ul>
+                        <li>#หาเพผู้</li>
+                        <li>#Generation</li>
+                    </ul>
+                </div>
+                <div class="panel-heading">
+                    <img class="[ img-circle pull-left ]" src="https://lh3.googleusercontent.com/-CxXg7_7ylq4/AAAAAAAAAAI/AAAAAAAAAQ8/LhCIKQC5Aq4/s46-c-k-no/photo.jpg" alt="Mouse0270" />
+                    <h3>Robert McIntosh</h3>
+                    <h5><span>Shared publicly</span> - <span>Jun 27, 2014</span> </h5>
+                </div>
+                <div class="panel-body">
+                    <p>Do people born in 2000 get to choose if they are Generation Y or Generation Z? How do you decide what generation you want to belong to?</p>
+                </div>
+                <div class="panel-footer">
+                    <button type="button" class="[ btn btn-default ] "><i class="fa fa-heart"></i></button>
+                
+                    <button type="button" class="[ btn btn-default ]">
+                        <i class="fa fa-share"></i>
+                    </button>
+                    <div class="input-placeholder col-md-8">Add a comment...</div>
+                </div>
+                <div class="panel-google-plus-comment">
+                    <img class="img-circle" src="https://lh3.googleusercontent.com/uFp_tsTJboUY7kue5XAsGA=s46" alt="User Image" />
+                    <div class="panel-google-plus-textarea">
+                        <textarea rows="4"></textarea>
+                        <button type="submit" class="[ btn btn-success disabled ]">Post comment</button>
+                        <button type="reset" class="[ btn btn-default ]">Cancel</button>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+        
+        
+    </div>
+</div>
+
+  </div>
+
+</div>
+    </div>
+    <div class="space"></div>
+<!-- end container -->
+</div>
+<!-- end main -->
+
+</body>
+
+    <script src="jquery/jquery-1.10.2.js" type="text/javascript"></script>
+  <script src="assets/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
+
+  <script src="bootstrap3/js/bootstrap.js" type="text/javascript"></script>
+  <script src="assets/js/gsdk-checkbox.js"></script>
+  <script src="assets/js/gsdk-radio.js"></script>
+  <script src="assets/js/gsdk-bootstrapswitch.js"></script>
+  <script src="assets/js/get-shit-done.js"></script>
+  <script src="assets/js/main.js"></script>
+  
+    <script src="assets/js/custom.js"></script>
+
+</html>
