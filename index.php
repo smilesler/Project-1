ad5890ba3592edc1f897c5efce75cef8a18feddb
SAIFAR BUNYANG <58024098@live.up.ac.th>
2018-05-24 12:26:13 +0700

    add

--- index.php
+++ index.php
@@ -66,28 +66,6 @@
           <li><a href="#team">แมว</a></li>
           <li><a href="dashboard.php">โปรไฟล์</a></li>
           <li><a href="login.php" style="color:red">Login</a></li>
-<<<<<<< HEAD
-          <li><a href="register.php" style="color:green">Register</a></li>
-          <!-- <li><a href="#features">Features</a></li>
-          <li><a href="#pricing">Pricing</a></li>
-          <li><a href="#team">Team</a></li>re
-          <li><a href="#gallery">Gallery</a></li>
-          <li class="menu-has-children"><a href="">Drop Down</a> -->
-           <!--  <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="menu-has-children"><a href="#">Drop Down 2</a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-              <li><a href="#">Drop Down 5</a></li>
-=======
           <li><a href="" style="color:green">Register</a></li>
           
           <li class="menu-has-children">
@@ -100,7 +78,6 @@
               <li><a href="#">แจ้งเตือน 3</a></li>
               <li><a href="#">แจ้งเตือน 4</a></li>
               <li><a href="#">แจ้งเตือน 5</a></li>
->>>>>>> 92bbe769b97dd19ba899e17a57b54b1071ec3d0a
             </ul>
           </li>
          
